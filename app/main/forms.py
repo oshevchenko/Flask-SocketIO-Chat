49ed81e3ca454859009cd4d8d505fb00c94ecf1c
oshevchenko <shevchenko.adb@gmail.com>
2021-05-23 14:42:18 +0300

    Replace deprecated Required with DataRequired.
    Signed-off-by: oshevchenko <shevchenko.adb@gmail.com>

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, SubmitField
-from wtforms.validators import Required
-
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     """Accepts a nickname and a room."""
-    name = StringField('Name', validators=[Required()])
-    room = StringField('Room', validators=[Required()])
+    name = StringField('Name', validators=[DataRequired()])
+    room = StringField('Room', validators=[DataRequired()])
     submit = SubmitField('Enter Chatroom')
